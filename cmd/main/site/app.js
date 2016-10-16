e5451a6aa00b1d33de2c27005d0d8a13d5055d0f
Milan Aleksic <milanaleksic@gmail.com>
2016-10-16 14:22:58 +0200

    working on #4: using vue.js instead of tutorial

--- cmd/main/site/app.js
+++ cmd/main/site/app.js
@@ -1,56 +1,54 @@
-$.extend(igor, {
-    identity: new $.Deferred(),
-
-    googleSignIn: function (googleUser) {
-        AWS.config.update({
-            region: igor.config.awsRegion,
-            credentials: new AWS.CognitoIdentityCredentials({
-                IdentityPoolId: igor.config.cognitoPoolId,
-                Logins: {
-                    'accounts.google.com': googleUser.getAuthResponse().id_token
-                }
-            })
-        });
-        function refresh() {
+var app = new Vue({
+    el: '#app',
+    data: $.extend(igor.config, {
+        email: "" 
+    }),
+    computed: {
+        welcomeMessage: function() {
+            if (this.email) {
+                return "Hi " + this.email
+            } else {
+                return "Checking..."
+            }
+        }
+    },
+    methods: {
+        // called by Google API when signing in has been done
+        googleSignIn: function (googleUser) {
+            AWS.config.update({
+                region: this.awsRegion,
+                credentials: new AWS.CognitoIdentityCredentials({
+                    IdentityPoolId: this.cognitoPoolId,
+                    Logins: {
+                        'accounts.google.com': googleUser.getAuthResponse().id_token
+                    }
+                })
+            });
+            this.updateCognitoServerToUseGoogleUser(googleUser);
+        },
+        // this is called by us when we detect credentials are not OK any more
+        refreshGoogleCredentials: function() {
             return gapi.auth2.getAuthInstance().signIn({
                 prompt: 'login'
             }).then(function (userUpdate) {
                 var creds = AWS.config.credentials;
                 var newToken = userUpdate.getAuthResponse().id_token;
                 creds.params.Logins['accounts.google.com'] = newToken;
-                return igor.awsRefresh();
-            });
+                return app.updateCognitoServerToUseGoogleUser(userUpdate);
+            })
+        },
+        updateCognitoServerToUseGoogleUser: function(googleUser) {
+            var appThis = this
+            AWS.config.credentials.refresh(function (err) {
+                if (err) {
+                    console.error("Error: ", err);
+                } else {
+                    appThis.email = googleUser.getBasicProfile().getEmail();
+                    appThis.id = AWS.config.credentials.identityId;  
+                }
+            })
         }
-        igor.awsRefresh().then(function (id) {
-            igor.identity.resolve({
-                id: id,
-                email: googleUser.getBasicProfile().getEmail(),
-                refresh: refresh
-            });
-        });
-    },
-
-    awsRefresh: function () {
-        var deferred = new $.Deferred();
-        AWS.config.credentials.refresh(function (err) {
-            if (err) {
-                deferred.reject(err);
-            } else {
-                deferred.resolve(AWS.config.credentials.identityId);
-            }
-        });
-        return deferred.promise();
-    },
-
-    appOnReady: function () {
-        igor.identity.done(function (profile) {
-            console.log("You are logged in as ", profile.email)
-        });
     }
-});
+})
 
-$(window).ready(igor.appOnReady);
-
-googleSignIn= function (googleUser) {
-    igor.googleSignIn(googleUser)
-}
+googleSignIn = app.googleSignIn;
