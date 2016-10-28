57a701792a6ec7f97ff6b7c1e7025d544ab89036
Milan Aleksic <milanaleksic@gmail.com>
2016-10-28 18:23:37 +0200

    increased allowed processing time, and log cleanup line added

--- cmd/main/cf/logs/get.sh
+++ cmd/main/cf/logs/get.sh
@@ -9,3 +9,4 @@ rm -rf $SCRIPT_DIR/temp/
 aws s3 sync s3://$BUCKET_FOR_LOGS/igor-export-task-output/ /data/$THIS_DIR_RELATIVE_TO_ROOT/temp/
 
 find $SCRIPT_DIR -name '*.gz' | xargs gunzip
+find $SCRIPT_DIR/temp/ -type f | sort | xargs cat | awk 'NF' | egrep -v '(START)|(END)|(REPORT)' | sort > $SCRIPT_DIR/temp/joined_all_significant.log
