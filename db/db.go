cc10ad4d1a7987b79cdcdc230653f1b9c32edd50
Milan Aleksic <milanaleksic@gmail.com>
2016-10-29 18:52:37 +0200

    bugfix: not enough precision in the time resolution used internally

--- db/db.go
+++ db/db.go
@@ -35,7 +35,7 @@ func (db *DB) SetLastCommunicationWith(userConfig *igor.UserConfig, username str
 		},
 		UpdateExpression:          aws.String("SET lastCommunication.#username = :new"),
 		ExpressionAttributeNames:  map[string]*string{"#username": aws.String(username)},
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{":new": {S: aws.String(moment.Format(time.RFC822))}},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{":new": {S: aws.String(moment.Format(time.RFC3339))}},
 	})
 	if err != nil {
 		_, err = db.dynamo.UpdateItem(&dynamodb.UpdateItemInput{
@@ -45,7 +45,7 @@ func (db *DB) SetLastCommunicationWith(userConfig *igor.UserConfig, username str
 			},
 			UpdateExpression: aws.String("SET lastCommunication = if_not_exists(lastCommunication, :new)"),
 			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{":new": {M: map[string]*dynamodb.AttributeValue{
-				username: &dynamodb.AttributeValue{S: aws.String(moment.Format(time.RFC822))},
+				username: &dynamodb.AttributeValue{S: aws.String(moment.Format(time.RFC3339))},
 			}}},
 		})
 		if err != nil {
@@ -58,7 +58,8 @@ func (db *DB) SetLastCommunicationWith(userConfig *igor.UserConfig, username str
 func (db *DB) GetAllConfigs() (allConfigs []*igor.UserConfig, err error) {
 	allConfigs = make([]*igor.UserConfig, 0)
 	resp, err := db.dynamo.Scan(&dynamodb.ScanInput{
-		TableName: aws.String("igor"),
+		TableName:      aws.String("igor"),
+		ConsistentRead: aws.Bool(true),
 	})
 	if err != nil {
 		return nil, err
@@ -75,7 +76,7 @@ func (db *DB) GetAllConfigs() (allConfigs []*igor.UserConfig, err error) {
 		if commMap, ok := item["lastCommunication"]; ok {
 			lastCommunicationMap := commMap.M
 			for user, lastTime := range lastCommunicationMap {
-				lastTimeParsed, err := time.Parse(time.RFC822, *lastTime.S)
+				lastTimeParsed, err := time.Parse(time.RFC3339, *lastTime.S)
 				if err != nil {
 					log.Fatalf("Last time %s couldn't be parsed, err: %+v", lastTime, err)
 					return nil, err
@@ -83,12 +84,12 @@ func (db *DB) GetAllConfigs() (allConfigs []*igor.UserConfig, err error) {
 				lastCommunication[user] = lastTimeParsed
 			}
 		}
-		parsedActiveFrom, err := time.Parse(time.RFC822, *item["activeFrom"].S)
+		parsedActiveFrom, err := time.Parse(time.RFC3339, *item["activeFrom"].S)
 		if err != nil {
 			log.Fatalf("Active from couldn't be parsed, err: %+v", err)
 			return nil, err
 		}
-		parsedActiveUntil, err := time.Parse(time.RFC822, *item["activeUntil"].S)
+		parsedActiveUntil, err := time.Parse(time.RFC3339, *item["activeUntil"].S)
 		if err != nil {
 			log.Fatalf("Active until couldn't be parsed, err: %+v", err)
 			return nil, err
